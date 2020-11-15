08d3bd188920b36bfe9096b4275aa3943a6265f7
jangascodingplace <janisgoesser92@gmail.com>
2020-11-15 23:31:11 +0100

    Create Login and Logout View and rewrite LoginForm

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 
 urlpatterns = [
-
+    path('signin', views.SignInView.as_view(), name="userSignInView"),
+    path('logout', LogoutView.as_view(), name="userLogoutView"),
 ]
---
+++ apps/user/views.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.views import LoginView
+from .forms import AuthenticationForm
+
+
+class SignInView(LoginView):
+    http_method_names = ["post", ]
+    form_class = AuthenticationForm
+    template_name = 'staticPages/index.html'
