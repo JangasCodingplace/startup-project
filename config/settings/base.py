b5ffde3f763714d9e067eedc48b88e18f7c265d1
jangascodingplace <janisgoesser92@gmail.com>
2020-11-16 00:21:32 +0100

    Implement Sendgrid

--- config/settings/base.py
+++ config/settings/base.py
@@ -105,3 +105,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'config', 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
