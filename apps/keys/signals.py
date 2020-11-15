1ad43678a183db4b234ff3e5ed2aa52f5a179eb4
jangascodingplace <janisgoesser92@gmail.com>
2020-11-16 00:26:39 +0100

    Email Integration in Frontend

--- apps/keys/signals.py
+++ apps/keys/signals.py
@@ -1,10 +1,17 @@
 from django.conf import settings
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.core.mail import send_mail
 from .models import Key
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_key_by_registration(*args, **kwargs):
     if kwargs['created']:
-        Key.objects.create(user=kwargs['instance'])
+        key = Key.objects.create(user=kwargs['instance'])
+        send_mail(
+            subject="Activation",
+            message=str(key.key),
+            from_email="janisgoesser92@gmail.com",
+            recipient_list=[key.user.email]
+        )
