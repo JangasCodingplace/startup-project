d0edcc123d7b7ab767f930ab50de9adcf508c255
jangascodingplace <janisgoesser92@gmail.com>
2020-11-16 00:00:46 +0100

    Fixing Linting issues on user views

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,16 +1,13 @@
 from django.contrib.auth.views import LoginView
 from django.views.generic import CreateView
 from .forms import AuthenticationForm, RegistrationForm
-from django.urls import reverse
 
 
 class SignInView(LoginView):
     http_method_names = ["post", ]
     form_class = AuthenticationForm
-    template_name = 'staticPages/index.html'
 
 
 class SignUpView(CreateView):
     form_class = RegistrationForm
-    template_name = 'staticPages/index.html'
     success_url = "/"
