8d0b751d35bc8d3ae7613d205114caaaae68dcf5
jangascodingplace <janisgoesser92@gmail.com>
2020-11-16 01:36:10 +0100

    Authenticate User after activation & delete key while using

--- apps/keys/views.py
+++ apps/keys/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth import login
 from .models import Key
 from django.views.generic import View
 
@@ -9,12 +10,17 @@ class KeyView(View):
         key = get_object_or_404(Key, key=key_pk)
         if not key.is_valid:
             raise Http404("Key is expired.")
+        key.delete()
         return key
+
+    def authentication(self, request, user):
+        login(request, user)
 
     def activation(self, request, key):
         user = key.user
         user.is_activated_by_key = True
         user.save()
+        self.authentication(request, user)
         return redirect("indexIndex")
 
     def get(self, request, key_pk, *args, **kwargs):
