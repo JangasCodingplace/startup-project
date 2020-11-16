9e6ec36c708c0871e28b1646de9c348206c1e077
jangascodingplace <janisgoesser92@gmail.com>
2020-11-16 01:49:57 +0100

    Redesign user urls

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,8 +4,24 @@ from . import views
 
 
 urlpatterns = [
-    path('signin', views.SignInView.as_view(), name="userSignInView"),
-    path('signup', views.SignUpView.as_view(), name="userSignUpView"),
-    path('logout', LogoutView.as_view(), name="userLogoutView"),
-    path('create-key', views.CreateKeyView.as_view(), name="userCreateKeyView"),
+    path(
+        'signin',
+        views.SignInView.as_view(),
+        name="userSignInView"
+    ),
+    path(
+        'signup',
+        views.SignUpView.as_view(),
+        name="userSignUpView"
+    ),
+    path(
+        'logout',
+        LogoutView.as_view(),
+        name="userLogoutView"
+    ),
+    path(
+        'create-key',
+        views.CreateKeyView.as_view(),
+        name="userCreateKeyView"
+    ),
 ]
