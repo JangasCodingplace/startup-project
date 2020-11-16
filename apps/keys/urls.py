4f00400946c4bf09a0201e0e3d31a365d5912083
jangascodingplace <janisgoesser92@gmail.com>
2020-11-16 01:12:17 +0100

    Add KeyView to URLs

---
+++ apps/keys/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path(
+        '<int:key_pk>',
+        views.KeyView.as_view(),
+        name="keysAccountActivationView"
+    )
+]
